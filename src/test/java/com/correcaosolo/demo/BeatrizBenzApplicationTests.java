dcc3145453c6fa1d57bc9c0eef409200640a36fd
Beatriz Benz <beatriz_benz@hotmail.com>
2021-03-13 20:00:08 -0300

    commit inicial add projeto

---
+++ src/test/java/com/correcaosolo/demo/BeatrizBenzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correcaosolo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatrizBenzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
